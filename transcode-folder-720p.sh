872d5ddc45a24f185ce44ec00000733130769be2
mjwells2002 <wellsmatthew2002+github@gmail.com>
2020-05-15 15:57:15 +0100

    add 720p and 1080p transcode folder scripts

---
+++ transcode-folder-720p.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#transcode-folder-480p.sh
+#usage transcode-folder-480p in-folder/ out-folder/
+
+in_folder=$1
+out_folder=$2
+
+for file in $in_folder/*
+do
+  if [ -f "${file}" ]
+  then
+    filename=$(basename $file)
+    echo trancodeing $filename to $out_folder/$filename-480p-trancode.mp4
+    ffmpeg -i $file -s 1280x720 -c:v libx264 -c:a aac -crf 21 -preset slow $out_folder/$filename-480p-transcode.mp4
+  fi
+done
