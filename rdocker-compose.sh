153e223993c548fcf14333f6de9885b39b789d98
mjwells2002 <wellsmatthew2002+github@gmail.com>
2020-05-15 17:13:17 +0100

    add comments

--- rdocker-compose.sh
+++ rdocker-compose.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#rdocker-compose.sh
+#usage rdocker-compose <docker-compose command>
+#allows the use of a seperate docker_host varible on docker-compose commands without always using it
 
 ARGS=$@ DOCKER_HOST=$RDOCKER_HOST bash -c 'docker-compose $ARGS'
--- rdocker.sh
+++ rdocker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+#rdocker.sh
+#usage rdocker <docker command>
+#allows the use of a seperate docker_host varible without messing with normal docker commands
 
 ARGS=$@ DOCKER_HOST=$RDOCKER_HOST bash -c 'docker $ARGS'
